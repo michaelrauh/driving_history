5301b3b86729dc919eda1e789fb6f1ff48c8a9a1
michaelrauh <michaelrauh@ymail.com>
2018-01-22 22:42:04 -0500

    first functionality in DrivingHistory opens up command line arguments

--- lib/driving_history.rb
+++ lib/driving_history.rb
@@ -1,2 +1,14 @@
+require_relative '../wrapper/command_line.rb'
+
+# Takes in a filename and produces a driving report
 class DrivingHistory
+  attr_accessor :command_line
+
+  def initialize
+    @command_line = CommandLine.new
+  end
+
+  def run
+    @command_line.args_array
+  end
 end
--- test/driving_history_test.rb
+++ test/driving_history_test.rb
@@ -1,8 +1,19 @@
 require 'minitest/autorun'
 require_relative '../lib/driving_history.rb'
+require_relative '../wrapper/command_line.rb'
 
 class DrivingHistoryTest < Minitest::Test
   def setup
+    @command_line = Minitest::Mock.new
+    @command_line.expect :args_array, ['foo.rb']
+
     @subject = DrivingHistory.new
+
+    @subject.command_line = @command_line
+  end
+
+  def test_run_method_reads_in_command_line_arguments
+    @subject.run
+    assert_mock @command_line
   end
 end
