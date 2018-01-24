d9431f397a028517f597a9a1051f869c4af86902
michaelrauh <michaelrauh@ymail.com>
2018-01-23 21:53:09 -0500

    introduce helpers to driving history test

--- test/driving_history_test.rb
+++ test/driving_history_test.rb
@@ -4,21 +4,11 @@ require_relative '../wrapper/command_line.rb'
 
 class DrivingHistoryTest < Minitest::Test
   def setup
-    @command_line = Minitest::Mock.new
-    @command_line.expect :args_array, ['foo.rb']
-
-    @file_helper = Minitest::Mock.new
-    @file_helper.expect :read, 'this is a file with words', ['foo.rb']
-
-    @model = Minitest::Mock.new
-    @model.expect :train, nil, ['this is a file with words']
-    @model.expect :report, 'this is a report'
+    initialize_mocks
 
     @subject = DrivingHistory.new
 
-    @subject.command_line = @command_line
-    @subject.file_helper = @file_helper
-    @subject.model = @model
+    inject_mocks
   end
 
   def test_run_method_reads_in_command_line_arguments
@@ -38,5 +28,25 @@ class DrivingHistoryTest < Minitest::Test
 
   def test_run_method_returns_model_report
     assert_equal('this is a report', @subject.run)
+  end
+
+  private
+
+  def initialize_mocks
+    @command_line = Minitest::Mock.new
+    @command_line.expect :args_array, ['foo.rb']
+
+    @file_helper = Minitest::Mock.new
+    @file_helper.expect :read, 'this is a file with words', ['foo.rb']
+
+    @model = Minitest::Mock.new
+    @model.expect :train, nil, ['this is a file with words']
+    @model.expect :report, 'this is a report'
+  end
+
+  def inject_mocks
+    @subject.command_line = @command_line
+    @subject.file_helper = @file_helper
+    @subject.model = @model
   end
 end
