7ba47b97dfb8ebeb7d44a1b0cc7d5449ffdd2063
michaelrauh <michaelrauh@ymail.com>
2018-01-23 21:24:39 -0500

    ask for a model to be built from file contents

--- lib/driving_history.rb
+++ lib/driving_history.rb
@@ -1,18 +1,22 @@
 require_relative '../wrapper/command_line.rb'
 require_relative '../wrapper/file_helper.rb'
+require_relative 'model.rb'
 
 # Takes in a filename and produces a driving report
 class DrivingHistory
   attr_accessor :command_line
   attr_accessor :file_helper
+  attr_accessor :model
 
   def initialize
     @command_line = CommandLine.new
     @file_helper = FileHelper.new
+    @model = Model.new
   end
 
   def run
     filename = @command_line.args_array.first
-    @file_helper.read(filename)
+    file_contents = @file_helper.read(filename)
+    @model.train(file_contents)
   end
 end
--- test/driving_history_test.rb
+++ test/driving_history_test.rb
@@ -10,10 +10,14 @@ class DrivingHistoryTest < Minitest::Test
     @file_helper = Minitest::Mock.new
     @file_helper.expect :read, 'this is a file with words', ['foo.rb']
 
+    @model = Minitest::Mock.new
+    @model.expect :train, nil, ['this is a file with words']
+
     @subject = DrivingHistory.new
 
     @subject.command_line = @command_line
     @subject.file_helper = @file_helper
+    @subject.model = @model
   end
 
   def test_run_method_reads_in_command_line_arguments
@@ -24,5 +28,10 @@ class DrivingHistoryTest < Minitest::Test
   def test_run_method_opens_file_according_to_name_passed
     @subject.run
     assert_mock @file_helper
+  end
+
+  def test_run_method_builds_a_model_from_file_contents
+    @subject.run
+    assert_mock @model
   end
 end
